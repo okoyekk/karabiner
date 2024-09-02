04444e7ba3530eed72860de9fae8769fa4c8668f
Kenechi Okoye <okoyekk@gmail.com>
2024-09-02 09:28:31 -0400

    Initial config + previously existing keybinds

--- rules.ts
+++ rules.ts
@@ -38,84 +38,113 @@ const rules: KarabinerRules[] = [
         ],
         type: "basic",
       },
-      //      {
-      //        type: "basic",
-      //        description: "Disable CMD + Tab to force Hyper Key usage",
-      //        from: {
-      //          key_code: "tab",
-      //          modifiers: {
-      //            mandatory: ["left_command"],
-      //          },
-      //        },
-      //        to: [
-      //          {
-      //            key_code: "tab",
-      //          },
-      //        ],
-      //      },
     ],
   },
+  {
+    description: "Shift + Esc to ~",
+    manipulators: [
+        {
+            from: {
+                key_code: "escape",
+                modifiers: {
+                    mandatory: [
+                        "shift"
+                    ]
+                }
+            },
+            to: [
+                {
+                    key_code: "grave_accent_and_tilde",
+                    modifiers: [
+                        "left_shift"
+                    ],
+                    repeat: true
+                }
+            ],
+            type: "basic"
+        }
+    ]
+  },
+  {
+    description: "Command + Esc to Command + ` (for window switching)",
+    manipulators: [
+        {
+            from: {
+                key_code: "escape",
+                modifiers: {
+                    mandatory: [
+                        "command"
+                    ]
+                }
+            },
+            to: [
+                {
+                    key_code: "grave_accent_and_tilde",
+                    modifiers: [
+                        "command"
+                    ]
+                }
+            ],
+            type: "basic"
+        }
+    ]
+  },
+  {
+    description: "Command + Shift + Esc to Command + ~ (for reverse window switching)",
+    manipulators: [
+        {
+            from: {
+                key_code: "escape",
+                modifiers: {
+                    mandatory: [
+                        "command",
+                        "shift"
+                    ]
+                }
+            },
+            to: [
+                {
+                    key_code: "grave_accent_and_tilde",
+                    modifiers: [
+                        "command",
+                        "shift"
+                    ]
+                }
+            ],
+            type: "basic"
+        }
+    ]
+  },
+
+
   ...createHyperSubLayers({
     spacebar: open(
-      "raycast://extensions/stellate/mxstbr-commands/create-notion-todo"
+      "https://google.com"
     ),
-    // b = "B"rowse
-    b: {
-      t: open("https://twitter.com"),
-      // Quarterly "P"lan
-      p: open("https://mxstbr.com/cal"),
-      y: open("https://news.ycombinator.com"),
-      f: open("https://facebook.com"),
+    // s = browse "S"ite
+    s: {
       r: open("https://reddit.com"),
-      h: open("https://hashnode.com/draft"),
+      f: open("https://www.bogleheads.org/index.php"),
+      y: open("https://www.youtube.com"),
+      m: open("https://monkeytype.com"),
     },
-    // o = "Open" applications
-    o: {
-      1: app("1Password"),
+    // a = open "A"pplications
+    a: {
       g: app("Google Chrome"),
-      c: app("Notion Calendar"),
-      v: app("Zed"),
-      d: app("Discord"),
-      s: app("Slack"),
-      e: app("Superhuman"),
-      n: app("Notion"),
+      s: app("Safari"),
+      c: app("Calendar"),
+      r: app("Reminders"),
+      m: app("Spark"),
+      n: app("Notes"),
       t: app("Terminal"),
-      // Open todo list managed via *H*ypersonic
-      h: open(
-        "notion://www.notion.so/stellatehq/7b33b924746647499d906c55f89d5026"
-      ),
-      z: app("zoom.us"),
-      // "M"arkdown (Reflect.app)
-      m: app("Reflect"),
-      r: app("Reflect"),
       f: app("Finder"),
-      // "i"Message
-      i: app("Texts"),
+      i: app("Messages"),
       p: app("Spotify"),
-      a: app("iA Presenter"),
-      // "W"hatsApp has been replaced by Texts
-      w: open("Texts"),
-      l: open(
-        "raycast://extensions/stellate/mxstbr-commands/open-mxs-is-shortlink"
-      ),
+      u: app("Sublime Text"),
+      v: app("Visual Studio Code"),
     },
 
-    // TODO: This doesn't quite work yet.
-    // l = "Layouts" via Raycast's custom window management
-    // l: {
-    //   // Coding layout
-    //   c: shell`
-    //     open -a "Visual Studio Code.app"
-    //     sleep 0.2
-    //     open -g "raycast://customWindowManagementCommand?position=topLeft&relativeWidth=0.5"
-
-    //     open -a "Terminal.app"
-    //     sleep 0.2
-    //     open -g "raycast://customWindowManagementCommand?position=topRight&relativeWidth=0.5"
-    //   `,
-    // },
-
-    // w = "Window" via rectangle.app
+    // w = "Window"
     w: {
       semicolon: {
         description: "Window: Hide",
@@ -126,13 +155,14 @@ const rules: KarabinerRules[] = [
           },
         ],
       },
-      y: rectangle("previous-display"),
-      o: rectangle("next-display"),
-      k: rectangle("top-half"),
-      j: rectangle("bottom-half"),
-      h: rectangle("left-half"),
-      l: rectangle("right-half"),
-      f: rectangle("maximize"),
+      // Currently use amethyst and already prefer existing rectangle shortcuts.
+      // y: rectangle("previous-display"),
+      // o: rectangle("next-display"),
+      // k: rectangle("top-half"),
+      // j: rectangle("bottom-half"),
+      // h: rectangle("left-half"),
+      // l: rectangle("right-half"),
+      // f: rectangle("maximize"),
       u: {
         description: "Window: Previous Tab",
         to: [
@@ -190,175 +220,91 @@ const rules: KarabinerRules[] = [
       },
     },
 
-    // s = "System"
-    s: {
-      u: {
-        to: [
-          {
-            key_code: "volume_increment",
-          },
-        ],
-      },
-      j: {
-        to: [
-          {
-            key_code: "volume_decrement",
-          },
-        ],
-      },
-      i: {
-        to: [
-          {
-            key_code: "display_brightness_increment",
-          },
-        ],
-      },
-      k: {
-        to: [
-          {
-            key_code: "display_brightness_decrement",
-          },
-        ],
-      },
-      l: {
-        to: [
-          {
-            key_code: "q",
-            modifiers: ["right_control", "right_command"],
-          },
-        ],
-      },
-      p: {
-        to: [
-          {
-            key_code: "play_or_pause",
-          },
-        ],
-      },
-      semicolon: {
-        to: [
-          {
-            key_code: "fastforward",
-          },
-        ],
-      },
-      e: open(
-        `raycast://extensions/thomas/elgato-key-light/toggle?launchType=background`
-      ),
-      // "D"o not disturb toggle
-      d: open(
-        `raycast://extensions/yakitrak/do-not-disturb/toggle?launchType=background`
-      ),
-      // "T"heme
-      t: open(`raycast://extensions/raycast/system/toggle-system-appearance`),
-      c: open("raycast://extensions/raycast/system/open-camera"),
-      // 'v'oice
-      v: {
-        to: [
-          {
-            key_code: "spacebar",
-            modifiers: ["left_option"],
-          },
-        ],
-      },
-    },
+    // // s = "System"
+    // s: {
+    //   u: {
+    //     to: [
+    //       {
+    //         key_code: "volume_increment",
+    //       },
+    //     ],
+    //   },
+    //   j: {
+    //     to: [
+    //       {
+    //         key_code: "volume_decrement",
+    //       },
+    //     ],
+    //   },
+    //   i: {
+    //     to: [
+    //       {
+    //         key_code: "display_brightness_increment",
+    //       },
+    //     ],
+    //   },
+    //   k: {
+    //     to: [
+    //       {
+    //         key_code: "display_brightness_decrement",
+    //       },
+    //     ],
+    //   },
+    //   l: {
+    //     to: [
+    //       {
+    //         key_code: "q",
+    //         modifiers: ["right_control", "right_command"],
+    //       },
+    //     ],
+    //   },
+    //   p: {
+    //     to: [
+    //       {
+    //         key_code: "play_or_pause",
+    //       },
+    //     ],
+    //   },
+    //   semicolon: {
+    //     to: [
+    //       {
+    //         key_code: "fastforward",
+    //       },
+    //     ],
+    //   },
+    // },
 
-    // v = "moVe" which isn't "m" because we want it to be on the left hand
-    // so that hjkl work like they do in vim
+    // v = "moVe" 
+    // couldn't wrap my head around hjkl so i use jikl
     v: {
-      h: {
+      j: {
         to: [{ key_code: "left_arrow" }],
       },
-      j: {
+      k: {
         to: [{ key_code: "down_arrow" }],
       },
-      k: {
+      i: {
         to: [{ key_code: "up_arrow" }],
       },
       l: {
         to: [{ key_code: "right_arrow" }],
       },
-      // Magicmove via homerow.app
-      m: {
-        to: [{ key_code: "f", modifiers: ["right_control"] }],
-        // TODO: Trigger Vim Easymotion when VSCode is focused
-      },
-      // Scroll mode via homerow.app
-      s: {
-        to: [{ key_code: "j", modifiers: ["right_control"] }],
-      },
-      d: {
-        to: [{ key_code: "d", modifiers: ["right_shift", "right_command"] }],
-      },
-      u: {
-        to: [{ key_code: "page_down" }],
-      },
-      i: {
-        to: [{ key_code: "page_up" }],
-      },
     },
 
-    // c = Musi*c* which isn't "m" because we want it to be on the left hand
-    c: {
-      p: {
-        to: [{ key_code: "play_or_pause" }],
-      },
-      n: {
-        to: [{ key_code: "fastforward" }],
-      },
-      b: {
-        to: [{ key_code: "rewind" }],
-      },
-    },
+    // // c = Musi*c* which isn't "m" because we want it to be on the left hand
+    // c: {
+    //   p: {
+    //     to: [{ key_code: "play_or_pause" }],
+    //   },
+    //   n: {
+    //     to: [{ key_code: "fastforward" }],
+    //   },
+    //   b: {
+    //     to: [{ key_code: "rewind" }],
+    //   },
+    // },
 
-    // r = "Raycast"
-    r: {
-      c: open("raycast://extensions/thomas/color-picker/pick-color"),
-      n: open("raycast://script-commands/dismiss-notifications"),
-      l: open(
-        "raycast://extensions/stellate/mxstbr-commands/create-mxs-is-shortlink"
-      ),
-      e: open(
-        "raycast://extensions/raycast/emoji-symbols/search-emoji-symbols"
-      ),
-      p: open("raycast://extensions/raycast/raycast/confetti"),
-      a: open("raycast://extensions/raycast/raycast-ai/ai-chat"),
-      s: open("raycast://extensions/peduarte/silent-mention/index"),
-      h: open(
-        "raycast://extensions/raycast/clipboard-history/clipboard-history"
-      ),
-      1: open(
-        "raycast://extensions/VladCuciureanu/toothpick/connect-favorite-device-1"
-      ),
-      2: open(
-        "raycast://extensions/VladCuciureanu/toothpick/connect-favorite-device-2"
-      ),
-    },
   }),
-  {
-    description: "Change Backspace to Spacebar when Minecraft is focused",
-    manipulators: [
-      {
-        type: "basic",
-        from: {
-          key_code: "delete_or_backspace",
-        },
-        to: [
-          {
-            key_code: "spacebar",
-          },
-        ],
-        conditions: [
-          {
-            type: "frontmost_application_if",
-            file_paths: [
-              "^/Users/mxstbr/Library/Application Support/minecraft/runtime/java-runtime-gamma/mac-os-arm64/java-runtime-gamma/jre.bundle/Contents/Home/bin/java$",
-            ],
-          },
-        ],
-      },
-    ],
-  },
 ];
 
 fs.writeFileSync(
@@ -374,6 +320,33 @@ fs.writeFileSync(
           complex_modifications: {
             rules,
           },
+          devices: [
+            {
+              disable_built_in_keyboard_if_exists: false,
+              fn_function_keys: [],
+              identifiers: {
+                  is_keyboard: true,
+                  is_pointing_device: false,
+                  product_id: 591,
+                  vendor_id: 1452
+              },
+              ignore: false,
+              manipulate_caps_lock_led: true,
+              simple_modifications: [
+                  {
+                      // Map home button to play/pause on keychron k6
+                      from: {
+                          key_code: "home"
+                      },
+                      to: [
+                          {
+                              consumer_key_code: "play_or_pause"
+                          }
+                      ]
+                  }
+              ]
+            }
+          ],
         },
       ],
     },
